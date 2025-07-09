007f6cf0104475383dfd547a7291729694bf1de7
Vipul Sahani <vipulsahani2006@gmail.com>
2025-07-09 10:01:27 +0530

    Update Task_02.java

--- Task_02.java
+++ Task_02.java
@@ -23,26 +23,31 @@ public class Task_02 {
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter total number of Subject: ");
-        int num_subject=sc.nextInt();
-        int totalmark=Input_marks(num_subject);// Calling the function for Input
-        double Average=Average(totalmark,num_subject);
-        char Grade;
-        if (Average >= 90) {
-            Grade = 'A';
-        } else if (Average >= 80) {
-            Grade = 'B';
-        } else if (Average >= 60) {
-            Grade = 'C';
-        } else if (Average >= 40) {
-            Grade = 'D';
-        } else {
-            Grade = 'F';
+        System.out.print("Enter number of student: ");
+        int number_of_student=sc.nextInt();
+        for(int i=1;i<=number_of_student;i++){
+            System.out.println("        Enter marks of student "+i);
+            System.out.print("Enter total number of Subject: ");
+            int num_subject=sc.nextInt();
+            int totalmark=Input_marks(num_subject);// Calling the function for Input
+            double Average=Average(totalmark,num_subject);
+            char Grade;
+            if (Average >= 90) {
+                Grade = 'A';
+            } else if (Average >= 80) {
+                Grade = 'B';
+            } else if (Average >= 60) {
+                Grade = 'C';
+            } else if (Average >= 40) {
+                Grade = 'D';
+            } else {
+                Grade = 'F';
+            }
+            System.out.println("Note --> Minimun passing percentage is 40\n");
+            System.out.println("--- RESULT ---");
+            System.out.println("Total mark: "+totalmark);
+            System.out.println("Average Percentage: "+Average);
+            System.out.println("Grade: "+Grade);
         }
-        System.out.println("Note --> Minimun passing percentage is 40\n");
-        System.out.println("--- RESULT ---");
-        System.out.println("Total mark: "+totalmark);
-        System.out.println("Average Percentage: "+Average);
-        System.out.println("Grade: "+Grade);
     }
 }
